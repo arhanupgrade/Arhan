9f6f1904fb9aa984ff71cbb8ac60f1c9e5adcfa7
arhanupgrade <108338786+arhanupgrade@users.noreply.github.com>
2022-07-22 13:55:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function rollDice()
 
   if(randomNumber1 > randomNumber2)
   {
-    document.querySelector("h1").innerHTML="Player 1 Wins!!";
+    document.querySelector("h1").innerHTML="Arhan Wins!!";
   }
   else if(randomNumber1 < randomNumber2)
   {
-    document.querySelector("h1").innerHTML="Player 2 Wins!!";
+    document.querySelector("h1").innerHTML="Arhan let you Win!!";
   }
   else
   {
