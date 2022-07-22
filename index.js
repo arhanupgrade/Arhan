e466242af32d42ff67cff96f4a78cc92201423e9
arhanupgrade <108338786+arhanupgrade@users.noreply.github.com>
2022-07-22 13:51:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function rollDice()
   var randomNumber1 = Math.round(Math.random()*5) + 1;
   var randomNumber2 = Math.round(Math.random()*5) + 1;
 
-  document.querySelector(".img1").src="images/dice"+randomNumber1+".png";
-  document.querySelector(".img2").src="images/dice"+randomNumber2+".png";
+  document.querySelector(".img1").src="dice"+randomNumber1+".png";
+  document.querySelector(".img2").src="dice"+randomNumber2+".png";
 
   if(randomNumber1 > randomNumber2)
   {
